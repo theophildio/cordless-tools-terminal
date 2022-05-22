8adc7c216c6d9bf5de0ad8eba7c3f1171e79b9ff
theophildio <dtheo.sangma@gmail.com>
2022-05-22 12:21:19 +0600

    Created Cordless Tools Terminal repo and added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#FE7E6D",
+					secondary: "#2F3A8F",
+					accent: "#FEECE9",
+					neutral: "#CCD1E4",
+					"base-100": "#FFFFFF",
+					info: "#3ABFF8",
+					success: "#36D399",
+					warning: "#FBBD23",
+					error: "#F87272",
+				},
+			},
+		],
+	},
+	plugins: [require("daisyui")],
+};
