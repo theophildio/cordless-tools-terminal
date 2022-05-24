4fc893362f0a9b4643ea5c6febc3b651b8858e0d
theophildio <dtheo.sangma@gmail.com>
2022-05-25 00:55:20 +0600

    Fixed some design issues on Purchase UI

--- src/Pages/Home/Tools/Purchase/PurchaseDetails.js
+++ src/Pages/Home/Tools/Purchase/PurchaseDetails.js
@@ -4,20 +4,22 @@ const PurchaseDetails = ({ purchase }) => {
 	const { toolName, img, description, quantity, price } = purchase;
 	return (
 		<div>
-			<div className="hero">
-				<div className="hero-content lg:w-3/4 justify-between flex-col lg:flex-row">
-					<div className="card bg-base-100 lg:w-1/2 shadow-md">
+			<div class="flex flex-col w-full lg:flex-row shadow-sm px-5">
+				<div class="grid lg:w-1/2 card bg-base-100 rounded-box place-items-center lg:p-12">
+				<div className="card lg:w-4/5 shadow-md">
 						<figure>
 							<img src={img} alt="20V Cordless Drill" className="w-1/2" />
 						</figure>
-						<div className="card-body">
+						<div className="card-body p-0 lg:p-5">
 							<h2 className="card-title">
 								{toolName}
 								<div className="badge badge-secondary">NEW</div>
 							</h2>
 							<p>{description}</p>
 							<div className="card-actions justify-between mt-5">
-								<div className="badge badge-outline bg-primary text-base-100 p-3">Quantity: {quantity}</div>
+								<div className="badge badge-outline bg-primary text-base-100 p-3">
+									Quantity: {quantity}
+								</div>
 								<div className="badge badge-outline bg-primary text-base-100 p-3">
 									Min. Order: 500 Pieces
 								</div>
@@ -29,7 +31,10 @@ const PurchaseDetails = ({ purchase }) => {
 							</div>
 						</div>
 					</div>
-					<div className="card w-full lg:w-1/2 shadow-md bg-base-100">
+				</div>
+				<div class="divider lg:divider-horizontal"></div>
+				<div class="grid flex-grow card rounded-box place-items-center lg:p-5">
+				<div className="card w-full shadow-md bg-base-100">
 						<div className="card-body p-5">
 							<div className="form-control">
 								<input
@@ -47,19 +52,28 @@ const PurchaseDetails = ({ purchase }) => {
 							</div>
 							<div className="form-control">
 								<input
+									type="text"
+									placeholder="Address"
+									className="input input-bordered"
+								/>
+							</div>
+							<div className="form-control">
+								<input
 									type="tel"
 									placeholder="Phone"
 									className="input input-bordered"
 								/>
 							</div>
 							<div className="form-control">
-								<textarea
-									className="textarea textarea-bordered"
-									placeholder="Your message"
-								></textarea>
+								<input
+									type="text"
+									placeholder="Min. Order: 500 pieces"
+									className="input input-bordered"
+								/>
 							</div>
+							<p className="text-lg font-semibold mt-4">Total Price: ${price}</p>
 							<div className="form-control mt-6">
-								<button className="btn btn-primary">Inquiry Now</button>
+								<button className="btn btn-primary">Place Order</button>
 							</div>
 						</div>
 					</div>
