f012263fb3ee9d5f17896150119f316d464e7e34
theophildio <dtheo.sangma@gmail.com>
2022-05-27 16:49:38 +0600

    Review added on UI dynamically

--- src/Pages/Reviews/ReviewCard.js
+++ src/Pages/Reviews/ReviewCard.js
@@ -1,27 +1,22 @@
 import React from "react";
 
 const ReviewCard = ({review}) => {
-  const {avatar, name, userReview} = review;
-  const showReview = userReview.slice(0, 85);
+  const {userPhotoURL, userName, reviewDetail, rating} = review;
 	return (
 		<>
 			<div className="card flex-col lg:flex-row items-center card-side bg-base-100 shadow-md p-5">
 				<div>
 					<div className="avatar">
 						<div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-							<img src={avatar} alt="Avatar"/>
+							<img src={userPhotoURL} alt="Avatar"/>
 						</div>
 					</div>
 				</div>
 				<div className="card-body">
-					<h2 className="card-title">{name}</h2>
-					<p>{showReview}...</p>
+					<h2 className="card-title">{userName}</h2>
+					<p>{reviewDetail}</p>
 					<div className="rating">
-            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            <p className="text-primary font-semibold">Ratings <span className="text-orange-500">{rating}</span> out of 5</p>
           </div>
 				</div>
 			</div>
--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,37 +1,29 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Spinner from "../SharedPages/Spinner";
 import ReviewCard from "./ReviewCard";
 
 const Reviews = () => {
-  const reviews = [
-    {
-      id: 1,
-      avatar: 'https://api.lorem.space/image/face?hash=3174',
-      name:'Josh McGaffigan',
-      userReview: 'Hilti Introduces A Compact Cordless Hammer Drill To The Nuron Platform We’ve seen some really cool stuff coming from the Hilti team ever since the launch of its Nuron battery platform. Many of the solid workhorse tools debuted on this system along with some newcomers.',
-      rating: '4'
-    },
-    {
-      id: 2,
-      avatar: 'https://api.lorem.space/image/face?hash=3174',
-      name:'Josh McGaffigan',
-      userReview: 'Hilti Introduces A Compact Cordless Hammer Drill To The Nuron Platform We’ve seen some really cool stuff coming from the Hilti team ever since the launch of its Nuron battery platform. Many of the solid workhorse tools debuted on this system along with some newcomers.',
-      rating: '4'
-    },
-    {
-      id: 3,
-      avatar: 'https://api.lorem.space/image/face?hash=3174',
-      name:'Josh McGaffigan',
-      userReview: 'Hilti Introduces A Compact Cordless Hammer Drill To The Nuron Platform We’ve seen some really cool stuff coming from the Hilti team ever since the launch of its Nuron battery platform. Many of the solid workhorse tools debuted on this system along with some newcomers.',
-      rating: '4'
-    },
-  ]
+  const {data: reviews, isLoading} = useQuery('reviews', () => 
+    fetch('http://localhost:5000/review', {
+      method: 'GET',
+      headers: {
+        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    }).then(res => res.json())
+  );
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
 	return (
 		<section className="px-4 lg:py-12 lg:px-12">
 			<h2 className="text-2xl lg:text-4xl font-bold text-secondary text-center mb-8 capitalize">Our Customer Reviews</h2>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
         {
           reviews.map( review => <ReviewCard 
-            key={review.id}
+            key={review._id}
             review={review}
           ></ReviewCard>)
         }
